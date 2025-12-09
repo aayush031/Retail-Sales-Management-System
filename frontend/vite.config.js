720d8fd80f08789b9f0d66b2b4949e3a6bcd2a4e
aayush031 <147484095+aayush031@users.noreply.github.com>
2025-12-09 17:50:32 +0530

    Update API proxy URL in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://retail-sales-management-system-1-qvxh.onrender.com",
     },
   },
 });
